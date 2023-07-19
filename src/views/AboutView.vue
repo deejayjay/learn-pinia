484ffe1b45667e4e749d24fc8fcee9fc58867fe7
Deepak Joy Jose <72120524+deejayjay@users.noreply.github.com>
2023-07-18 20:33:53 -0600

    Cleans up the initial project.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,20 @@
+<script setup>
+
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+.about {
+  text-align: center;
+}
+
+h1 {
+  margin: 1rem 0;
+  font-size: 3rem;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>Home</h1>
+  </div>
 </template>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+
+h1 {
+  margin: 1rem 0;
+  font-size: 3rem;
+}
+</style>
